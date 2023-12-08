de1f8405aa1462616cbf20b825b81189aead56bc
nati <seifunatinael@gmail.com>
2023-12-08 10:42:47 +0300

    initial commit

---
+++ tf/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.20.0"
+    }
+  }
+}
+
+provider "aws" {
+
+       }
+
+  resource "aws_s3_bucket" "myterr-s3-bucket-00" {
+  bucket = var.bucket_name
+
+   tags = {
+    
+   name = var.bucket_tag
+  }
+}
