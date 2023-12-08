8ac6544530fde6abd7067d2f70cde41f4c12bea2
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-12-08 11:45:49 +0300

    Terraform state file

--- tf/main.tf
+++ tf/main.tf
@@ -7,17 +7,26 @@ terraform {
       version = "5.20.0"
     }
   }
+   backend "s3" {
+
+    bucket         = "terraform-state-key20231117084134961500000001"
+    key            = "Project-Kilole/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+
+  }
 }
 
 provider "aws" {
 
        }
 
-  resource "aws_s3_bucket" "myterr-s3-bucket-00" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "myterr-s3-bucket-00" {
+bucket = var.bucket_name
 
-   tags = {
-    
-   name = var.bucket_tag
+  tags = {
+    name = var.bucket_tag
   }
 }
+
