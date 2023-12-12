c500c8ef09b689673b1b774a019acfb1b9dec127
nati <seifunatinael@gmail.com>
2023-12-12 10:25:19 +0300

    configure iot_rule

--- tf/main.tf
+++ tf/main.tf
@@ -41,7 +41,15 @@ resource "aws_kinesis_firehose_delivery_stream" "demo_delivery_stream" {
   extended_s3_configuration {
     role_arn   = aws_iam_role.firehose.arn
     bucket_arn = aws_s3_bucket.kilole01.arn
+    buffering_interval = 60
+    buffering_size = 1
+    prefix = "kilole-data/"
 
+    cloudwatch_logging_options {
+      enabled             = true
+      log_group_name      = "/aws/kinesisfirehose/kilole-project-delivery"
+      log_stream_name     = "extended_s3_log_stream"
+    }
 }
 }
 resource "aws_iam_role" "firehose" {
@@ -94,4 +102,37 @@ EOF
 resource "aws_iam_role_policy_attachment" "firehose_s3" {
   role       = aws_iam_role.firehose.name
   policy_arn = aws_iam_policy.firehose_s3.arn
+}
+
+############################################
+# Iot rule config
+############################################
+
+resource "aws_iot_topic_rule" "rule" {
+  name        = "project_kilole"
+  #description = "Example rule"
+  enabled     = true
+  sql         = "SELECT * FROM '/kilole-topic/#'"
+  sql_version = "2016-03-23"
+  
+    firehose {
+      delivery_stream_name = aws_kinesis_firehose_delivery_stream.demo_delivery_stream.name
+      role_arn            = "arn:aws:iam::480053995985:role/service-role/kilole_iot_rule_role"
+      separator = "\n"
+    }
+  }
+
+
+#############################################
+# cloudwatch log group and stream
+#############################################
+
+resource "aws_cloudwatch_log_group" "demo_firebose_log_group" {
+  name = "/aws/kinesisfirehose/kilole-project-delivery"
+ 
+}
+
+resource "aws_cloudwatch_log_stream" "demo_firebose_log_stream" {
+  name           = "extended_s3_log_stream"
+  log_group_name = aws_cloudwatch_log_group.demo_firebose_log_group.name
 }
