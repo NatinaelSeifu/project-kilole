ff37d77b2ef43818ba937ef788d35090706b9790
nati <seifunatinael@gmail.com>
2023-12-12 11:28:19 +0300

    hide resource variables

--- tf/variables.tf
+++ tf/variables.tf
@@ -9,15 +9,35 @@ variable "bucket_name" {
 #   type=string 
 # }
 
-# variable "state_bucket_name" {
-#   type = string
+variable "state_bucket_name" {
+  type = string
   
-# }
-# variable "state_key_path" {
-#   type = string
-  
-# }
-# variable "dynamodb_table_lock" {
-#   type = string
-  
-# }
+ }
+
+ variable "firehose_name" {
+   type = string
+ }
+
+ variable "cwlog_name" {
+   type = string
+ }
+
+ variable "cwstream" {
+   type = string
+ }
+
+ variable "iot_rule01" {
+   type = string
+ }
+
+variable "iot_rule_sql" {
+  type = string
+}
+
+variable "iot_rule_role_firehose" {
+  type = string
+}
+
+variable "s3_prefix" {
+  type = string
+}
